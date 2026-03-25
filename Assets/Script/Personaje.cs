b7761b06c00cc8ab22d2150b67a2d224edf5c3f3
LucasFerre777 <lucassferre04@gmail.com>
2026-03-25 13:27:30 +0100

    Estoy aprendiendo a scriptear

--- Assets/Script/Personaje.cs
+++ Assets/Script/Personaje.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Personaje : MonoBehaviour
 {
+
+    int miNumero = 1;
+
+    float miNumero = 0.8f;
+
+    string miCadenaDeTexto = "Hola, esto es una cadena de texto";
+
+    bool miBoolean = true;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("inicio");
     }
 
     // Update is called once per frame
@@ -13,4 +23,18 @@ public class Personaje : MonoBehaviour
     {
         Debug.Log("Hola");
     }
+
+    int sumar()
+    {
+        
+    }
+
+    //Esto es un mensaje para los humanos
+
+    /*
+    todo esto
+    tambien es un mensaje
+    para los humanos 
+    */
+
 }
