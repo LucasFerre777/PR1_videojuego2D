39597350993c428a2a35b4f17ff94cbd50a15df7
LucasFerre777 <lucassferre04@gmail.com>
2026-03-25 14:35:49 +0100

    Aún hay errores de script

--- Assets/Script/Personaje.cs
+++ Assets/Script/Personaje.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 public class Personaje : MonoBehaviour
@@ -5,28 +6,50 @@ public class Personaje : MonoBehaviour
 
     int miNumero = 1;
 
-    float miNumero = 0.8f;
+    float miNumeroFlotante = 0.8f;
 
     string miCadenaDeTexto = "Hola, esto es una cadena de texto";
 
     bool miBoolean = true;
 
 
+    public Vector3 inicioPersonaje = new Vector3(1,1,0);
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("inicio");
+        SumaEntreDecenas = Sumar(10,20,3.8f);
+
+        Debug.Log(this.transform.position.x);
+        Debug.Log(SumaEntreDecenas);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("Hola");
-    }
 
-    int sumar()
+        /*
+        if(miVida == 3 && miVida >= 0 && miNumeroFlotante == 0.8f)
+        {
+            Debug.Log("Estoy vivo")
+        }
+        else
+        {
+            Debug.Log("Estoy muerto")
+        }
+        */
+    }
+  
+
+    float Sumar(int num1, int num2, float num3)
     {
         
+        float suma = num1 + num2 + num3;
+
+        return suma;
     }
 
     //Esto es un mensaje para los humanos
@@ -36,5 +59,6 @@ public class Personaje : MonoBehaviour
     tambien es un mensaje
     para los humanos 
     */
-
+    
 }
+
