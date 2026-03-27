a07a8c078672b80eef89c749a817e4e6d725abbf
LucasFerre777 <lucassferre04@gmail.com>
2026-03-27 12:58:06 +0100

    He añadido movimiento1

--- Assets/Script/Personaje.cs
+++ Assets/Script/Personaje.cs
@@ -1,60 +1,32 @@
 
+using System.Runtime.InteropServices.Marshalling;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Personaje : MonoBehaviour
 {
-
-    int miNumero = 1;
-
-    float miNumeroFlotante = 0.8f;
-
-    string miCadenaDeTexto = "Hola, esto es una cadena de texto";
-
-    bool miBoolean = true;
-
-
+    public float velocidad = 0.5f ;
     public Vector3 inicioPersonaje = new Vector3(1,1,0);
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(this.transform.position.x);
+        
+        this.transform.position = inicioPersonaje;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hola");
-
-        /*
-        if(miVida == 3 && miVida >= 0 && miNumeroFlotante == 0.8f)
-        {
-            Debug.Log("Estoy vivo")
-        }
-        else
-        {
-            Debug.Log("Estoy muerto")
-        }
-        */
-    }
-  
-
-    int Sumar(int num1, int num2)
-    {
         
-        int suma = num1 + num2;
+        Vector2 moveInput = InputSystem.actions["Move"].ReadValue<Vector2>();
 
-        return suma;
+        this.transform.Translate(moveInput.x*velocidad,moveInput.y*velocidad, 0);
+        
+
     }
 
-    //Esto es un mensaje para los humanos
-
-    /*
-    todo esto
-    tambien es un mensaje
-    para los humanos 
-    */
-    
 }
 
